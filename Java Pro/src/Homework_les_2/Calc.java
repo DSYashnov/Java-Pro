d663abe29d0a7eb648a98fc0fabb67e13066e95b
Stanislav Yashnov <s.yashnov@gmail.com>
2023-12-08 01:48:33 +0200

    Homework2

--- Java Pro/src/Homework_les_2/Calc.java
+++ Java Pro/src/Homework_les_2/Calc.java
@@ -1,12 +1,11 @@
 package Homework_les_2;
-import java.util.Scanner;
 
 public class Calc {
-    static Scanner s = new Scanner(System.in);
+
     public static void main(String[] args) {
         // Вводимо два числа
-        double a = s.nextDouble();
-        double b = s.nextDouble();
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
 
         //Виводимо середнє арифметичне двох чисел
         System.out.println((a + b)/2);
